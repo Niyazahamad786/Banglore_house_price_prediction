cc9d72ae8a704a921084c3a62fd478dd9e0fbf6b
Niyazahamad786 <niyazahamad195@gmail.com>
2020-12-17 18:40:02 -0800

    Areas

--- bhp/forms.py
+++ bhp/forms.py
@@ -1,5 +1,4 @@
 from flask_wtf import FlaskForm
-#from flask_wtf.file import FileField,FileAllowed
 from wtforms import  SubmitField,SelectField,FloatField
 from wtforms.validators import DataRequired,Length
 import json
@@ -10,6 +9,7 @@ class MainForm(FlaskForm):
         for i in  x:
             i=i.upper()
             lis.append((i,i))
+        lis=[('Other','Other')]+lis
     Total_sqft=FloatField('Total Sqft*',validators=[DataRequired(), Length(min=2, max=6)])
     Bedroom=SelectField('Bedroom*',validators=[DataRequired()], choices=[('0','None'),('1','1'),('2','2'),('3','3'),('4','4'),('5','5'),('6','6'),('7','7'),('8','8')])
     Bath=SelectField('Bath*',validators=[DataRequired()], choices=[('0','None'),('1','1'),('2','2'),('3','3'),('4','4'),('5','5'),('6','6'),('7','7'),('8','8'),('9','9'),('10','10')])
